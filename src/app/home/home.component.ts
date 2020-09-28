11e2d6c94aa2e3f91128129d8d9c36e9bf5f5dae
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-09-28 10:19:07 +0100

    Add search to nav bar (WIP)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(res => console.log({ res })
-    )
+    // this.productService.getProducts().subscribe(res => console.log({ res })
+    // )
   }
 }
