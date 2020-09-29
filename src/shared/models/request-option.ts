859cba6acd0e3e9ad0697d4055211126568e4514
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-09-29 10:47:40 +0100

    Add other params to interface

--- src/shared/models/request-option.ts
+++ src/shared/models/request-option.ts
@@ -1,5 +1,15 @@
-export interface RequestOption{
-  publisherId:string;
-  locale:string;
+export interface RequestOption {
+  publisherId: string;
+  locale: string;
+  site?: string;
+  shipCountry?: string;
+  term?: string;
+  start?: string;
+  perPage?: number;
+  brandId?: string;
+  categoryId?: string;
+  sellerId?: string;
+  priceRangeId?: string;
+  onlyMaProducts?: string;
 
 }
