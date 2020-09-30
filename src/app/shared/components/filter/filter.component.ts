1481d8e2f629b8bdb1720a16df6d60689da91d38
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-09-30 17:16:55 +0100

    Expand all panels on page load

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatAccordion } from '@angular/material/expansion';
 import { Category, Seller } from 'src/shared/models/product';
 import { Brand, PriceRange } from './../../../../shared/models/product';
 
@@ -8,19 +9,26 @@ import { Brand, PriceRange } from './../../../../shared/models/product';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
-export class FilterComponent implements OnInit {
+export class FilterComponent implements OnInit, AfterViewInit {
   @Input() categories: Category[] = [];
   @Input() brands: Brand[] = [];
   @Input() sellers: Seller[] = [];
   @Input() priceRanges: PriceRange[] = [];
   filterForm: FormGroup;
 
+  @ViewChild('accordion') accordion: MatAccordion;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.filterForm = this.fb.group({
 
-    })
+    });
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.accordion.openAll();
+    });
   }
 
 }
